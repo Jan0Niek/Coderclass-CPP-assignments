8341aee49641264f26b9f447b87ae4cc9d6b2d3b
Jan_Niek <yannick.eleveld@coderclass.nl>
2023-12-12 15:29:12 +0100

    hanoi beetje tower

--- towers of hanoi/hanoi.cpp
+++ towers of hanoi/hanoi.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <vector>
+
+std::vector<int> towerSizeInput()
+{
+    std::vector<int> A;
+    int H;
+
+    std::cout << "How many disks should the tower consist of?\n";
+    std::cin >> H;
+    if (std::cin.fail() || H <= 0)
+    {
+        std::cout << "invalid input. \n";
+        return std::vector<int>{-1};
+    }
+    
+    for (int i = 0; i < H; i++)
+    {
+        A.push_back(i+1);
+    }
+    return A;
+}
+
+void solve(int H, std::vector<int> A)
+{
+    std::vector<int> B;
+    std::vector<int> C;
+    if (A.size() % 2 == 0)
+    {
+        // beweeg de steen van a naar b
+    } //else van a naar c
+    
+}
 
 int main()
 {
+
+
 
     return 0;
 }
