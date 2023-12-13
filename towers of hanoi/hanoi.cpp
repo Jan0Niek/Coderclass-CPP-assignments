e0b7bef722b5439c3df27caffdef5872751737e7
Jan_Niek <yannick.eleveld@coderclass.nl>
2023-12-13 10:22:21 +0100

    hanoi klopt niet

--- towers of hanoi/hanoi.cpp
+++ towers of hanoi/hanoi.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
-#include <vector>
 
-std::vector<int> towerSizeInput()
+int towerSizeInput()
 {
-    std::vector<int> A;
     int H;
-
     std::cout << "How many disks should the tower consist of?\n";
     std::cin >> H;
     if (std::cin.fail() || H <= 0)
     {
         std::cout << "invalid input. \n";
-        return std::vector<int>{-1};
+        return -1;
     }
-    
-    for (int i = 0; i < H; i++)
-    {
-        A.push_back(i+1);
-    }
-    return A;
+    return H;
 }
 
-void solve(int H, std::vector<int> A)
+int solve(int H, int newH)
 {
-    std::vector<int> B;
-    std::vector<int> C;
-    if (A.size() % 2 == 0)
+    if (H % 2 == 0)
     {
-        // beweeg de steen van a naar b
-    } //else van a naar c
+        std::cout << "move disk " << H-newH << " from a to b" << std::endl;
+    } else {
+        std::cout << "move disk " << H-newH << " from a to c" << std::endl;
+    }
+    if (newH==0)
+    {
+        return 1;
+    }
+    
+
+    solve(H, newH-1);
     
 }
 
 int main()
 {
-
-
+    int H = towerSizeInput();
+    if (H == -1) return -1;
+    solve(H, H-1);
+    
 
     return 0;
 }
