a2c9780e6d064f4409cb1523ec1a5f25de188d85
Jan_Niek <yannick.eleveld@coderclass.nl>
2024-01-15 13:33:32 +0100

    stack af

--- implementing a stack/stack.cpp
+++ implementing a stack/stack.cpp
@@ -1,63 +1,98 @@
 #include <iostream>
+#include <limits>
 #include <vector>
 
 class Stack {
    public:
-       Stack();
-
-   	    bool isEmpty() const{
-            // returns true if stack has no elements stored
-            return elements.empty();
-        }
-
-   	    int top() const{
-            // returns element from top of the stack
-            // throws std::runtime_error("stack is empty")
-            if (isEmpty())
-            {
-                throw std::runtime_error("stack is empty");
-            }
-            return elements.front();
-        }
-
-       int pop(){
-            // returns element from top of the stack and removes it
-            // throws std::runtime_error("stack is empty")
-            if (isEmpty())
-            {
-                throw std::runtime_error("stack is empty");
-            }
-            int top = elements.front();
-            elements.erase(elements.begin());
-            return top;
-       }
-
-   	    void push(int val){
-            // puts a new element on top of the stack
-            elements.insert(elements.begin(), val);
-        }
-
-       std::string toString() const{
-            // returns the contents of the stack in the format
-            // [top,next,...,bottom]
-            // e.g.: push(1), push(2), toString() -> [2,1]
-            std::string returnString = "["
-            for (int i = 0; i < elements.size(); i++)
-            {
-                returnString = returnString + elements.at(i) + "," //fix
-            }
-            
-       }
-
-
-   private:
-       std::vector<int> elements;
+        Stack();
+        bool isEmpty() const;
+        int top() const;
+        int pop();
+        void push(int i);
+        std::string toString() const;
+    private:
+        std::vector<int> elements;
 };
 
+Stack::Stack(){
 
-int main()
-{
+}
 
+bool Stack::isEmpty() const{
+    return elements.empty();
+}
+
+int Stack::top() const{
+    if (isEmpty())
+    {
+        throw std::runtime_error("stack is empty");
+    }
+    return elements.front();
+}
+
+int Stack::pop(){
+    if (isEmpty())
+    {
+        throw std::runtime_error("stack is empty");
+    }
+    int top = elements.front();
+    elements.erase(elements.begin());
+    return top;
+}
+
+void Stack::push(int val){
+    elements.insert(elements.begin(), val);
+}
+
+std::string Stack::toString() const{
+    std::string returnString = "[";
+    for (int i = 0; i < elements.size(); i++)
+    {
+        returnString = returnString + std::to_string(elements.at(i)) + ",";
+    }
+    if (!elements.empty()) returnString.erase(returnString.size()-1);
+    returnString = returnString + "]";
+    return returnString;
+}
+
+
+
+int main() {
+    Stack stack;
+    
+    while (true) {
+        try {
+            std::cout << "stack> ";
+            
+            std::string command;
+            std::cin >> command;
+            
+            if ( (command.compare("end") == 0) || std::cin.eof() ){
+                break;
+            } else if (command.compare("top") == 0) {
+                std::cout << stack.top() << std::endl;
+            } else if (command.compare("pop") == 0) {
+                std::cout << stack.pop() << std::endl;
+            } else if (command == "push") {
+                if ( std::cin.eof() ) break;
+                int i;
+                std::cin >> i;
+                bool failed = std::cin.fail();
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                if ( failed ){
+                  throw std::runtime_error("not a number");
+                }
+                stack.push(i);
+            } else if ( command.compare("list") == 0){
+                std::cout << stack.toString() << std::endl;
+            } else {
+                throw std::runtime_error("invalid command");
+            }
+        } catch (std::runtime_error& e) {
+            std::cout << "error: " << e.what() << std::endl;
+        }
+    }
     
     return 0;
 }
