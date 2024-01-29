aeaea588a9115160c82aeb0c4de8341a8390c478
Jan_Niek <yannick.eleveld@coderclass.nl>
2024-01-29 13:48:22 +0100

    worms

--- maze generator/maze.cpp
+++ maze generator/maze.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+
+class Cell{
+    public:
+        Cell();
+
+    private:
+
+};
+Cell::Cell(){
+
+}
+
+class Maze{
+    public:
+        Maze();
+
+    private:
+
+};
+Maze::Maze(){
+
+}
+
 int main(int argc, char argv[])
 {
 
