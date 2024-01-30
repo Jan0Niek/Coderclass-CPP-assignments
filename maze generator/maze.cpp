811c7698c749cd8b9af39c46539368d0c3550207
Jan_Niek <yannick.eleveld@coderclass.nl>
2024-01-30 15:30:28 +0100

    oh-oh

--- maze generator/maze.cpp
+++ maze generator/maze.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stack>
+#include <vector>
 
 
 class Cell{
@@ -6,10 +8,15 @@ class Cell{
         Cell();
 
     private:
+        int x;
+        int y;
+        bool visited;
 
 };
 Cell::Cell(){
-
+    x = -1;
+    y = -1;
+    visited = false;
 }
 
 class Maze{
@@ -17,13 +24,44 @@ class Maze{
         Maze();
 
     private:
+        int width;
+        int height;
+        std::vector<std::vector<Cell>> board; //kut!
+        
 
 };
 Maze::Maze(){
 
 }
 
-int main(int argc, char argv[])
-{
+class Pathfinder{
+    public:
+        Pathfinder();
+    private:
 
+};
+Pathfinder::Pathfinder(){
+
+}
+
+int main(int argc, char *argv[])
+{
+    int height;
+    int width;
+
+    if (argc == 3)
+    {
+        height = std::stoi(argv[1]);
+        width = std::stoi(argv[2]);
+    } else if (argc == 4)
+    {
+        std::cout << "haha, nahh";
+        srand(std::stoi(argv[3])); 
+    } else {
+        return -1;
+    }
+
+
+
+    return 0;
 }
