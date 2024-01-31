1076a7ac5a3d7297787fdf4b0f9087d359eef4bc
Jan_Niek <yannick.eleveld@coderclass.nl>
2024-01-31 10:21:37 +0100

    oh-oh, lastig

--- maze generator/maze.cpp
+++ maze generator/maze.cpp
@@ -11,27 +11,43 @@ class Cell{
         int x;
         int y;
         bool visited;
+        char opening; // north east south west
 
 };
 Cell::Cell(){
     x = -1;
     y = -1;
     visited = false;
+    opening = 'x';
 }
 
 class Maze{
     public:
         Maze();
+        void setSize(int width, int height);
 
     private:
-        int width;
-        int height;
-        std::vector<std::vector<Cell>> board; //kut!
+        int width = -1;
+        int height = -1;
+        std::vector<Cell> board; 
         
 
 };
 Maze::Maze(){
 
+}
+void Maze::setSize(int width, int height){
+    this->width = width;
+    this->height = height;
+    for (int i = 0; i < width*height; i++)
+    {
+        if (i % width == 0) std::cout << '\n';
+        std::cout << "hokje ";
+        
+        Cell cell;
+        board.push_back(cell);
+    }
+    
 }
 
 class Pathfinder{
@@ -48,18 +64,17 @@ int main(int argc, char *argv[])
 {
     int height;
     int width;
+    Maze maze;
 
-    if (argc == 3)
-    {
-        height = std::stoi(argv[1]);
-        width = std::stoi(argv[2]);
-    } else if (argc == 4)
-    {
-        std::cout << "haha, nahh";
-        srand(std::stoi(argv[3])); 
-    } else {
-        return -1;
-    }
+    if (argc != 3 && argc != 4) return -1;
+    else if (argc == 4) srand(std::stoi(argv[3])); 
+    
+    height = std::stoi(argv[1]);
+    width = std::stoi(argv[2]);
+    maze.setSize(width, height);
+    maze.generate();
+    Pathfinder.dothings
+
 
 
 
